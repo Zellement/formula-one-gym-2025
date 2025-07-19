ccd5f123569e80e25bd191f539aef82ac3c9a350
Dan Farrow <dan@zellement.com>
2025-07-19 16:49:38 +0100

    add gsap

--- storyblok/ContentWhyChooseUs.vue
+++ storyblok/ContentWhyChooseUs.vue
@@ -1,6 +1,12 @@
 <template>
     <div class="@container">
-        <ul class="grid grid-cols-1 @xs:grid-cols-2">
+        <ul
+            v-gsap.delay-200.stagger.once.whenVisible.from="{
+                opacity: 0,
+                x: -20
+            }"
+            class="grid grid-cols-1 @xs:grid-cols-2"
+        >
             <li
                 v-for="(item, index) in defaultWhyChooseUs"
                 :key="index"
