6592d951cdf900ed5e3332e311ab6839c533eb8c
Dan Farrow <dan@zellement.com>
2025-07-27 13:51:19 +0100

    add more

--- storyblok/AtomIconTitlePair.vue
+++ storyblok/AtomIconTitlePair.vue
@@ -1,7 +1,34 @@
 <template>
-    <li v-editable="blok" class="flex flex-row items-center gap-2">
-        <Icon :name="blok.icon" :size="blok.size" />
-        <span>{{ blok.name }}</span>
+    <li v-editable="blok" class="flex w-full flex-col">
+        <a
+            v-if="blok.link?.linktype === 'url'"
+            :href="blok.link?.cached_url || blok.link?.url"
+            class="flex flex-row items-center gap-2 text-orange-500 underline underline-offset-1"
+        >
+            <Icon :name="blok.icon" :size="blok.size" />
+            <span>{{ blok.name }}</span>
+        </a>
+        <a
+            v-else-if="blok.link?.linktype === 'email'"
+            :href="`mailto:${blok.link?.cached_url || blok.link?.url}`"
+            class="flex flex-row items-center gap-2 text-orange-500 underline underline-offset-1"
+        >
+            <Icon :name="blok.icon" :size="blok.size" />
+            <span>{{ blok.name }}</span>
+        </a>
+        <nuxt-link
+            v-else-if="blok.link?.linktype === 'story'"
+            :href="`/${blok.link.story?.full_slug}`"
+            class="flex flex-row items-center gap-2 text-orange-500 underline underline-offset-1"
+        >
+            <Icon :name="blok.icon" :size="blok.size" />
+            <span>{{ blok.name }}</span>
+        </nuxt-link>
+
+        <span v-else class="flex flex-row items-center gap-2 text-orange-500">
+            <Icon :name="blok.icon" :size="blok.size" />
+            <span>{{ blok.name }}</span>
+        </span>
     </li>
 </template>
 
---
+++ storyblok/ContentIconTextPair.vue
@@ -0,0 +1,13 @@
+<template>
+    <StoryblokComponent
+        v-for="nestedBlok in blok.body"
+        :key="nestedBlok._uid"
+        :blok="nestedBlok"
+    />
+</template>
+
+<script lang="ts" setup>
+import type { ContentIconTextPairStoryblok } from '~/types/storyblok-component-types'
+
+defineProps<{ blok: ContentIconTextPairStoryblok }>()
+</script>
