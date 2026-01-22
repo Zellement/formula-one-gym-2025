a954c088a766703d3fadbd9ea3da9e1d9425fd59
Dan Farrow <dan@zellement.com>
2026-01-22 20:52:30 +0000

    error page

--- error.vue
+++ error.vue
@@ -1,8 +1,10 @@
 <template>
-    <div class="flex flex-col">
+    <div
+        class="flex h-full min-h-screen flex-col items-center justify-center p-4 text-center"
+    >
         <h1>Sorry!</h1>
         <p>The page you are looking for does not exist.</p>
-        <nuxt-link to="/">Return to Home</nuxt-link>
+        <nuxt-link class="underline" to="/">Go to the Home Page</nuxt-link>
     </div>
 </template>
 
