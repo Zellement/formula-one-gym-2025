7c006113f612b706487d426fbbece1a2ed2c9221
Dan Farrow <dan@zellement.com>
2026-02-22 10:11:19 +0000

    remove map, fix stars

--- components/Molecules/TotalReviews.vue
+++ components/Molecules/TotalReviews.vue
@@ -10,19 +10,33 @@
                 opacity: 0,
                 x: -20
             }"
-            class="text-copper-500 inline-flex items-center justify-start gap-1.5"
+            class="relative inline-flex items-center justify-start gap-1.5"
         >
-            <Icon
-                v-for="i in 5"
-                :key="i"
-                name="ic:outline-star"
-                size="20"
-                :class="starsClasses"
-            />
-
+            <div class="relative">
+                <Icon
+                    v-for="i in 5"
+                    :key="i"
+                    name="i-mdi-star-outline"
+                    size="20"
+                />
+                <div class="absolute inset-0 z-10">
+                    <Icon
+                        v-for="i in 4"
+                        :key="i"
+                        name="i-mdi-star"
+                        size="20"
+                        :class="starsClasses"
+                    />
+                    <Icon
+                        name="i-mdi-star-half"
+                        size="20"
+                        :class="starsClasses"
+                    />
+                </div>
+            </div>
             <span
-                v-if="totalReviews && rating"
-                class="font-sans text-sm text-white"
+                v-if="totalReviews && rating && isHomePage"
+                class="font-sans text-sm text-white lining-nums"
                 :class="starsClasses"
             >
                 <span class="text-[1.2em]">{{ rating }}</span>
@@ -51,5 +65,9 @@ const rating: ComputedRef<number | null> = computed(() => {
     return (
         storyblokStore?.siteOptions?.reviewsGoogle?.place_info?.rating || null
     )
+})
+
+const isHomePage = computed(() => {
+    return useRoute().path === '/'
 })
 </script>
